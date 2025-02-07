a015521b2b453f813d91e7ca05b8f968ed4f8a30
Dawid Smulewicz <dawid@necodeo.com>
2025-02-07 21:06:38 +0100

    homepage setting

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use Illuminate\Support\ServiceProvider;
 
 use A17\Twill\Facades\TwillNavigation;
 use A17\Twill\View\Components\Navigation\NavigationLink;
+use A17\Twill\Facades\TwillAppSettings;
+use A17\Twill\Services\Settings\SettingsGroup;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +29,9 @@ class AppServiceProvider extends ServiceProvider
         );
         TwillNavigation::addLink(
             NavigationLink::make()->forModule('menuLinks')->title('Menu')
+        );
+        TwillAppSettings::registerSettingsGroup(
+            SettingsGroup::make()->name('homepage')->label('Homepage')
         );
     }
 }
