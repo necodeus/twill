c78f8c405d854dc9e5e2dc15b8279351467cb640
Dawid Smulewicz <dawid@necodeo.com>
2025-02-07 21:12:07 +0100

    move crops to block_editor!

--- config/twill.php
+++ config/twill.php
@@ -3,19 +3,19 @@
 return [
     'block_editor' => [
         'use_twill_blocks' => [],
-    ],
-    'crops' => [
-        'highlight' => [
-            'desktop' => [
-                [
-                    'name' => 'desktop',
-                    'ratio' => 16 / 9,
+        'crops' => [
+            'highlight' => [
+                'desktop' => [
+                    [
+                        'name' => 'desktop',
+                        'ratio' => 16 / 9,
+                    ],
                 ],
-            ],
-            'mobile' => [
-                [
-                    'name' => 'mobile',
-                    'ratio' => 1,
+                'mobile' => [
+                    [
+                        'name' => 'mobile',
+                        'ratio' => 1,
+                    ],
                 ],
             ],
         ],
