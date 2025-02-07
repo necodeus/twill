7a8ae8c79c27067e179c9f149fe68ea4ebed6a80
Dawid Smulewicz <dawid@necodeo.com>
2025-02-07 19:52:39 +0100

    pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 
+use A17\Twill\Facades\TwillNavigation;
+use A17\Twill\View\Components\Navigation\NavigationLink;
+
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        TwillNavigation::addLink(
+            NavigationLink::make()->forModule('pages')
+        );
     }
 }
